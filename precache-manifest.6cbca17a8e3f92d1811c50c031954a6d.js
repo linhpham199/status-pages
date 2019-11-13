dfab8247f0ecfa045a2eac4c5a2beb7b76314013
Linh Pham <linhpt199@gmail.com>
2019-11-13 23:27:38 +0200

    Updates

---
+++ precache-manifest.6cbca17a8e3f92d1811c50c031954a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14c7dfc189f6d6d88ecf40f53d8062e",
+    "url": "/status-pages/index.html"
+  },
+  {
+    "revision": "0002391e3cebe0d3a963",
+    "url": "/status-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "067f9d4e883dcc8b8c90",
+    "url": "/status-pages/static/js/2.95375af9.chunk.js"
+  },
+  {
+    "revision": "0002391e3cebe0d3a963",
+    "url": "/status-pages/static/js/main.d3586273.chunk.js"
+  },
+  {
+    "revision": "05cc142fa5e97e92d86c",
+    "url": "/status-pages/static/js/runtime-main.7ce11b31.js"
+  }
+]);
